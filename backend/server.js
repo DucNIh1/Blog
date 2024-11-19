68050af80c4c844a7efef1b667a6168b15abf441
Do Duc Ninh <ducninh10x03@gmail.com>
2024-11-19 18:19:44 +0700

    update env

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 app.use(express.json());
 app.use(cookieParser());
 
